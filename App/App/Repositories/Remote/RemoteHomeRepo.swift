e34a9bd4312a15fbc0aea83849b1a0ca90fe4850
Edric D <dat.duong@ascendcorp.com>
2021-06-07 00:37:16 +0700

    update necessary

--- App/App/Repositories/Remote/RemoteHomeRepo.swift
+++ App/App/Repositories/Remote/RemoteHomeRepo.swift
@@ -6,13 +6,14 @@ import Utility
 class RemoteHomeRepo: AppBaseRepo, IHomeRepo {
 
     func getHomeService() -> Observable<[Home]> {
-        let service = invokeService(HomeApiService.self)
         
-        service.getListHomeInfo(complete: {
-            let result = ListConverter(HomeDataToHome()).convert(from: $0)
-            print(result)
-            // hanle
-        })
-        return .empty()
+        let service = invokeService(HomeApiService.self)
+        return Observable.create { obsever in
+            service.getListHomeInfo(complete: {
+                let result = ListConverter(HomeDataToHome()).convert(from: $0)
+                obsever.onNext(result)
+            })
+            return Disposables.create()
+        }
     }
 }
